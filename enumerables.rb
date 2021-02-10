8241101af186f7daec4efa17399c5ea0f0c888d1
notrexxx <notrexx15@gmail.com>
2021-02-10 16:22:55 -0400

    Created skeleton

--- enumerables.rb
+++ enumerables.rb
@@ -1,8 +1,52 @@
 module Enumerable
+    
+    
+    #1
     def my_each
      
     end
 
+    #2
+    def my_each_with_index
+     
+    end
+
+    #3
+    def my_select
+     
+    end
+
+    #4
+    def my_all?
+     
+    end
+
+    #5
+    def my_any?
+     
+    end
+
+    #6
+    def my_none?
+     
+    end
+
+    #7
+    def my_map
+     
+    end
+
+    #8
+    def my_inject
+     
+    end
+
+    #9
+    def multiply_els
+     
+    end
+
+    
 
 
     #end of module
