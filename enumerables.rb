47931f99c27c8787725926058bfbfe393da693f0
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-02-11 13:16:24 -0400

    Implement my_each enumerable method

--- enumerables.rb
+++ enumerables.rb
@@ -1,53 +1,48 @@
 module Enumerable
-    
-    
-    #1
     def my_each
-     
+      for i in arr do
+        yield i
+      end
+
+      arr
     end
 
-    #2
     def my_each_with_index
-     
+
     end
 
     #3
     def my_select
-     
+
     end
 
     #4
     def my_all?
-     
+
     end
 
     #5
     def my_any?
-     
+
     end
 
     #6
     def my_none?
-     
+
     end
 
     #7
     def my_map
-     
+
     end
 
     #8
     def my_inject
-     
+
     end
 
     #9
     def multiply_els
-     
+
     end
-
-    
-
-
-    #end of module
   end
