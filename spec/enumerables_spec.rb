923ab8bc0b7189eb7877227e4e0dc1c796aa79e5
notrexxx <notrexx15@gmail.com>
2021-02-24 19:49:13 -0400

    add #multiply_els, #my_each_with_index and #My_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,4 +1,4 @@
-require './enumerables.rb'
+require './enumerables'
 
 describe 'Enumerable' do
   let(:arr) { [1, 2, 3, 4, 5] }
@@ -11,5 +11,31 @@ describe 'Enumerable' do
     end
   end
 
-  
+  describe '#my_each_with_index' do
+    it 'returns a enumerator if no block is given' do
+      expect(arr.my_each_with_index).to be_an Enumerator
+      expect(range.my_each_with_index).to be_an Enumerator
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns a enumerator if no block is given' do
+      expect(arr.my_select).to be_an Enumerator
+      expect(range.my_select).to be_an Enumerator
+    end
+
+    it 'returns array' do
+      expect(arr.my_select { |n| n < 1 }).to eq(arr.select { |n| n < 1 })
+    end
+
+    it 'returns array::range' do
+      expect(range.my_select { |n| n == 3 }).to eq(range.select { |n| n == 3 })
+    end
+  end
+end
+
+describe 'multiply_els' do
+  it 'multiply numbers in array' do
+    expect(multiply_els([1, 2, 3])).to eq(1 * 2 * 3)
+  end
 end
